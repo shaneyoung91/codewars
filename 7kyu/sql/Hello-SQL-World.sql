e7b82b28ed38dfe695d6e229d4cc61b7e8604111
shaneyoung91 <shaneadam.young@gmail.com>
2024-02-09 12:45:28 -0800

    SQL Solutions to 7kyu

---
+++ 7kyu/sql/Hello-SQL-World.sql
@@ -0,0 +1,9 @@
+-- Hello SQL!
+
+-- Return a table with a single column named Greeting with the phrase 'hello world!'
+
+-- Please use Data Manipulation Language and not Data Definition Language to solve this Kata
+
+
+--  My Solution
+SELECT 'hello world!' AS "Greeting"
